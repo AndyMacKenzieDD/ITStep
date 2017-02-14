6793daf014f42553a236c1a4b59e221d562cfa64
Andy <AndyMacKenzideDD@icloud.com>
2017-02-14 13:41:23 +0300

    Add HomeWork Kredit Variables

---
+++ Python/Python Kredit.py
@@ -0,0 +1,11 @@
+Summ = int(input('How many want to take the money: '))
+Proc = float(input('By what percentage you take money: '))/100
+Years = int(input('For how many years: '))
+
+Month = (Summ * Proc * (1 + Proc) * Years) / (12 * ((1 + Proc) * Years - 1))
+m = Years * 12
+ALL = Month * m
+
+print(Month,"$")
+print(ALL, "$")
+print("Press enter to continue..")
